c4a58e4ffcece30559d41636add7eb3c0e9aa00b
ErbaZZ <weerawat.paw@gmail.com>
2021-10-08 00:51:02 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,219 @@
+require('dotenv').config();
+
+import Web3 from 'web3';
+import abiDecode from 'abi-decoder';
+import ERC20 from './abi/ERC20.json';
+import Pair from './abi/Pair.json';
+import WUSDMaster from './abi/WUSDMaster.json';
+import Router from './abi/Router.json';
+import ContractAddress from './ContractAddress.json';
+
+// ====== ENV ======
+
+const RPC_URL = process.env.RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const GAS_BASE = process.env.GAS_BASE;
+const GAS_LIMIT = process.env.GAS_LIMIT;
+
+// ====== CONSTANTS ======
+
+const BN = Web3.utils.BN;
+const PATH_USDT_BUSD_WUSD = [ContractAddress["USDT"], ContractAddress["BUSD"], ContractAddress["WUSD"]];
+const PATH_WEX_USDT = [ContractAddress["WEX"], ContractAddress["USDT"]];
+
+// ====== CONNECTION ======
+
+const provider = new Web3.providers.WebsocketProvider(RPC_URL, {
+    clientConfig: {
+        keepalive: true,
+        keepaliveInterval: 60000,
+    },
+    reconnect: {
+        auto: true,
+        delay: 12000,
+        onTimeout: true,
+        maxAttempts: 10
+    }
+});
+
+const web3 = new Web3(provider);
+const account = web3.eth.accounts.wallet.add(PRIVATE_KEY);
+provider.on('connect', () => {
+    console.log("Connected!");
+});
+provider.on('error', err => {
+    console.log(`WSS Error: ${err.message}`);
+});
+provider.on('end', async (err) => {
+    console.log(`WSS Connection Stopped!`);
+});
+
+// ====== CONTRACTS ======
+
+const wusdMaster = new web3.eth.Contract(WUSDMaster, ContractAddress["WUSDMaster"]);
+const waultRouter = new web3.eth.Contract(Router, ContractAddress["WaultSwapRouter"]);
+const wusd = new web3.eth.Contract(ERC20, ContractAddress["WUSD"]);
+const usdt = new web3.eth.Contract(ERC20, ContractAddress["USDT"]);
+const busd = new web3.eth.Contract(ERC20, ContractAddress["BUSD"]);
+const wex = new web3.eth.Contract(ERC20, ContractAddress["WEX"]);
+const wusdbusdLP = new web3.eth.Contract(Pair, ContractAddress["WUSDBUSDLP"]);
+const usdtwexLP = new web3.eth.Contract(Pair, ContractAddress["USDTWEXLP"]);
+
+// ====== VARIABLES ======
+
+let currentBlock = 0;
+let isTransactionOngoing = false;
+
+// ====== FUNCTIONS ======
+
+const swapToken = async (routerContract, amountIn, amountOutMin, path, gasPrice) => {
+    await routerContract.methods.swapExactTokensForTokens(amountIn, amountOutMin, path, account.address, Math.floor(Date.now() / 1000) + 60).send({
+        gasPrice: gasPrice.toString(),
+        gas: GAS_LIMIT,
+        from: account.address
+    }).on('transactionHash', function (transactionHash) {
+        console.log(`Swapping Token: ${transactionHash} (${web3.utils.fromWei(gasPrice, 'gwei')} Gwei)`);
+    }).on('receipt', (receipt) => {
+        console.log("Token Swapping Success!");
+    }).on('error', (err) => {
+        throw err;
+    });
+};
+
+const redeem = async (wusdAmount, gasPrice) => {
+    await wusdMaster.methods.redeem(wusdAmount).send({
+        gasPrice: gasPrice.toString(),
+        gas: GAS_LIMIT,
+        from: account.address
+    }).on('transactionHash', function (transactionHash) {
+        console.log(`Redeeming: ${transactionHash} (${web3.utils.fromWei(wusdAmount, 'ether')} WUSD)`);
+    }).on('receipt', (receipt) => {
+        console.log("Token Redeeming Success!");
+    }).on('error', (err) => {
+        throw err;
+    });
+};
+
+const claimUsdt = async (minUSDT, gasPrice) => {
+    await wusdMaster.methods.claimUsdt(minUSDT).send({
+        gasPrice: gasPrice.toString(),
+        gas: GAS_LIMIT,
+        from: account.address
+    }).on('transactionHash', function (transactionHash) {
+        console.log(`Claiming: ${transactionHash}`);
+    }).on('receipt', (receipt) => {
+        console.log("Token Claiming Success!");
+    }).on('error', (err) => {
+        throw err;
+    });
+};
+
+async function main() {
+	const blockSubscription = web3.eth.subscribe('newBlockHeaders');
+    // const pendingSubscription = web3.eth.subscribe('pendingTransactions');
+
+    blockSubscription.on('data', async (block, error) => {
+        // Skip on redeeming
+        if (isTransactionOngoing) return;
+
+        currentBlock = block.number;
+        console.log("----");
+        console.warn(new Date().toLocaleString());
+        console.log(`Block\t: ${currentBlock}`);
+        // Get USDT Balance
+
+        const usdtBalance = new BN(await usdt.methods.balanceOf(account.address).call());
+        // const usdtBalance = new BN(web3.utils.toWei("10", 'ether'));
+
+        if (usdtBalance.lte(new BN(0))) return;
+
+        // Get WUSD Reserves/Total Supply
+        const wusdSupply = new BN(await wusd.methods.totalSupply().call());
+        
+        // Get WEX Balance in WUSDMaster
+        const wexBalance = new BN(await wex.methods.balanceOf(ContractAddress["WUSDMaster"]).call());
+
+        // Quote USDT to WUSD from redeem
+        const wusdSwapAmounts = await waultRouter.methods.getAmountsOut(usdtBalance, PATH_USDT_BUSD_WUSD).call();
+        const wusdAmount = new BN(wusdSwapAmounts[wusdSwapAmounts.length - 1]);
+        
+        const wexToSwap = wexBalance.mul(wusdAmount).div(wusdSupply);
+        
+        const wexSwapAmounts = await waultRouter.methods.getAmountsOut(wexToSwap, PATH_WEX_USDT).call();
+        const usdtFromWex = new BN(wexSwapAmounts[wexSwapAmounts.length - 1]);
+        const usdtFromRedeem = wusdAmount.mul(new BN(895)).div(new BN(1000)).add(usdtFromWex);
+       
+        console.log(`Balance\t: ${web3.utils.fromWei(usdtBalance, 'ether')}`)
+        console.log(`Redeem\t: ${web3.utils.fromWei(usdtFromRedeem, 'ether')}`)
+
+        const profitPercent = usdtFromRedeem.mul(new BN(100)).div(new BN(usdtBalance)).toNumber() - 100;
+        console.log(`Profit\t: ${profitPercent}%`)
+
+        // Skip on low profit
+        if (profitPercent < 3) return;
+
+        isTransactionOngoing = true;
+
+        await swapToken(waultRouter, usdtBalance, usdtBalance.mul(new BN(98)).div(new BN(100)), PATH_USDT_BUSD_WUSD, GAS_BASE);
+        const wusdBalance = new BN(await wusd.methods.balanceOf(account.address).call());
+        await redeem(wusdBalance, GAS_BASE);
+        await claimUsdt("0", GAS_BASE);
+        isTransactionOngoing = false;
+
+        const afterUsdtBalance = new BN(await usdt.methods.balanceOf(account.address).call());
+
+        if (afterUsdtBalance.lt(usdtBalance)) {
+            console.warn("Bad Redeem!");
+            return;
+        }
+
+        const actualProfit = afterUsdtBalance.sub(usdtBalance);
+        const actualProfitPercent = actualProfit.mul(new BN(10000)).div(usdtBalance).toNumber();
+        console.log(`Actual Profit:\t${web3.utils.fromWei(actualProfit, 'ether')} USDT (${actualProfitPercent/100}%)`)
+    }).on("error", (err) => {
+        console.error(err.message);
+        isTransactionOngoing = false;
+    });
+
+    // pendingSubscription.on('data', async (txHash) => {
+    //     // Do nothing when we have no pending transaction
+    //     if (!isTransactionOngoing) return null;        
+
+    //     try {
+    //         const pendingTx = await web3.eth.getTransaction(txHash);
+
+    //         // Do nothing when it is own transaction
+    //         if (pendingTx.from === OWNER_ADDRESS) return null;
+
+    //         // Do nothing when the transaction is not for the router contract
+    //         if (pendingTx.to !== CAKE_ROUTER_ADDRESS) return null;
+
+    //         // Decode transaction input
+    //         const decodedData = abiDecoder.decodeMethod(pendingTx.input);
+
+    //         // Do nothing when the function is not swapExactTokensForTokens
+    //         if (decodedData.name !== "swapExactTokensForTokens") return null;
+
+    //         // Do nothing when the path is not the current swap path
+    //         if (decodedData.params[2].value[0] !== latestTransactionData.swapPath[0] || decodedData.params[2].value[1] !== latestTransactionData.swapPath[1]) return null;
+
+    //         console.log(`Found Pending Tx:\n\tFrom: ${pendingTx.from}\n\tGas: ${ethers.utils.formatUnits(pendingTx.gasPrice, 'gwei')}`);
+
+    //         let monitorGas = parseInt(pendingTx.gasPrice);
+    //         // Do nothing when the gas price is less than our transaction
+    //         if (monitorGas < latestTransactionGas) return null;
+
+    //         // Update gas
+    //         updateGas(monitorGas);
+    //     } catch (err) {
+    //         // console.log("Error on gas update")
+    //     }
+    // });
+}
+
+main().then(async () => {
+    // do nothing
+}).catch((err) => {
+    console.error(err);
+    process.exit(1337);
+});
